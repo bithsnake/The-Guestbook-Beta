5d68506761d3ef390717793e6f718b2a7e3f7631
Kimmo <39192814+bithsnake@users.noreply.github.com>
2021-11-10 11:03:19 +0100

    Updated project with a new DB connection string

---
+++ Migrations/20211110094046_UpdateDataBase_With_Models.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kursmoment3.Migrations
+{
+    public partial class UpdateDataBase_With_Models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
